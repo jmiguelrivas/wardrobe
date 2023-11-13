695b9974b39f758a7179cead4a15cd104b1562f8
Miguel Rivas <jemiguelrivas@gmail.com>
2023-11-12 21:12:38 -0800

    fixing loop

--- scripts/db/collections.js
+++ scripts/db/collections.js
@@ -1,12 +1,14 @@
 export default [
   {
     id: 0,
+    name: 'side',
     collection: 1,
     style: 'black-red',
     zoom: 1,
-    top: '10%',
-    left: '10%',
-    name: 'side',
+    position: {
+      top: '10%',
+      left: '10%',
+    },
     brand: 'Cofandy',
     description: 'Mesh Shorts',
   },
--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,10 @@
 import "./modules/alpinejs-3.min.js";
 import collections from "./db/collections.js";
+
+Alpine.store('selected', {
+    default: []
+})
+
+Alpine.store('db', {
+    collections: collections
+})
